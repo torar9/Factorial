2424287878c45b32dd22a1d576efb7685551a582
Tomáš Silber <tomas.silber@outlook.cz>
2018-07-12 22:27:51 +0200

    Added comments to help understand code

--- Factorial/bignumber.cpp
+++ Factorial/bignumber.cpp
@@ -5,20 +5,30 @@ BigNumber::~BigNumber()
     array.clear();
 }
 
+/*
+* Creates BigNumber with [number] value
+* Example: BigNumber(128) -> *this->[array] = vec[1, 2, 8]
+*/
 BigNumber::BigNumber(int number)
 {
     array.resize(lenght(number));
     setVector(number);
 }
 
+/*
+* CreatesBigNumber with zero value
+*/
 BigNumber::BigNumber()
 {
     array.resize(1);
     array[0] = 0;
 }
 
+/*
+* Returns number of digits in [number]
+*/
 int BigNumber::lenght(int number)
-{ 
+{
     int lenght = 0;
     while(number)
     {
@@ -29,11 +39,17 @@ int BigNumber::lenght(int number)
     return lenght;
 }
 
+/*
+* Fills *this->[array] vector with digits from [number]
+*/
 void BigNumber::setVector(int number)
 {
     setVector(number, this->array);
 }
 
+/*
+* Fills &[array] vector from second parameter with digits from [number]
+*/
 void BigNumber::setVector(int number, vector<int> &array)
 {
     for(int i = 0, dec = 10, leng = lenght(number); i < leng; i++)
@@ -43,6 +59,9 @@ void BigNumber::setVector(int number, vector<int> &array)
     }
 }
 
+/*
+* Fills &[array] array from second parameter with digits from [number]
+*/
 void BigNumber::setArray(int number, int *array)
 {
     for(int i = 0, dec = 10, leng = lenght(number); i < leng; i++)
@@ -52,10 +71,13 @@ void BigNumber::setArray(int number, int *array)
     }
 }
 
+/*
+* Multiply *this->[array] vector with number
+*/
 void BigNumber::multiply(int number)
 {
     int Nlengh = lenght(number);
-    
+
     vector<int>num;
     num.resize(Nlengh);
     setVector(number, num);
@@ -73,6 +95,10 @@ void BigNumber::multiply(int number)
     array = arrHelp;
 }
 
+/*
+* Shifts every element in vector bigger than 10 in order to represent each digits of number
+* Example: fixVector([5, 12, 2]) -> [6, 2, 2]
+*/
 void BigNumber::fixVector(vector<int> &array)
 {
     string::size_type i = 0, carry = 0;
@@ -89,6 +115,9 @@ void BigNumber::fixVector(vector<int> &array)
     }
 }
 
+/*
+* Prints to console digits stored in *this->[array]
+*/
 void BigNumber::writeNumber()
 {
     for(int i = (array.size() - 1); i >= 0; i--)
